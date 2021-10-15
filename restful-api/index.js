95ed30b27f6305309e56cd50bb12c66950563850
Han Tuerker <hantuerker@Hans-Mac-mini.local>
2021-10-15 22:28:48 +0300

    add https support

--- restful-api/index.js
+++ restful-api/index.js
@@ -3,12 +3,37 @@
 */
 
 const http = require('http');
+const https = require('https');
 const url = require('url');
+const fs = require('fs');
 const StringDecoder = require('string_decoder').StringDecoder;
 
 const config = require('./config');
 
-const server = http.createServer(function(req, res) {
+// Instantiate the HTTP server
+const httpServer = http.createServer(function(req, res) {
+    unifiedServer(req, res);
+});
+
+// Start the HTTP server
+httpServer.listen(config.httpPort, function() {
+    console.log('The HTTP server is listening on port: ', config.httpPort, ' in ', config.envName, ' mode.');
+});
+
+// Instantiate the HTTPS server
+const httpsServerOptions = {
+    'key': fs.readFileSync('./https/key.pem'),
+    'cert': fs.readFileSync('./https/cert.pem'),
+};
+const httpsServer = https.createServer(httpsServerOptions, function(req, res) {
+    unifiedServer(req, res);
+})
+// Start the HTTPS server
+httpsServer.listen(config.httpsPort, function() {
+    console.log('The HTTPS server is listening on port: ', config.httpsPort, ' in ', config.envName, ' mode.');
+})
+
+const unifiedServer = function(req, res) {
     const parsedUrl = url.parse(req.url, true);
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/$/g, '');
@@ -44,11 +69,7 @@ const server = http.createServer(function(req, res) {
             console.log('Returning this response', statusCode, payloadString);
         });
     });
-});
-
-server.listen(config.port, function() {
-    console.log('Server is listening on port: ', config.port, ' in ', config.envName, ' mode.');
-});
+}
 
 const handlers = {};
 
